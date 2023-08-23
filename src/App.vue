d8d1b798744c3e95ffd3725e08a16fdd39d1d5b5
devkevinpak <kevin.pak@obigo.com>
2023-08-23 15:00:35 +0900

    이거 참 브랜치 명이 달라고 오리진저장소에 저장은아무대나가능??

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <div class="">add sub main</div>
   <div class="">submit test 2</div>
+  <div class="">submit test 3</div>
 </template>
 
 <script>
